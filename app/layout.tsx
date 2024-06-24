ddb3c8dc0a128bec5a84157f52176e0f9b171598
Rhythm Sharma <rrhythmsharma@gmail.com>
2024-06-24 05:47:08 +0530

    fix: cookie name for prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
 
 function getToken() {
   const cookieStore = cookies();
-  // return cookieStore?.get("__Secure-next-auth.session-token")?.value || "";
-  return cookieStore?.get("next-auth.session-token")?.value || "";
+  return cookieStore?.get("__Secure-next-auth.session-token")?.value || "";
+  // return cookieStore?.get("next-auth.session-token")?.value || "";
 }
 
 export default async function RootLayout({
